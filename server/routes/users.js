a5f64a2d6ef5c59a984cd6c647a0eba93b3fe2eb
21500706 <21500706@handong.edu>
2021-08-13 13:06:29 +0900

    test commit

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,6 +8,7 @@ router.route("/").get((req, res) => {
 });
 
 router.route("/add").post((req, res) => {
+	console.log("test");
 	const username = req.body.username;
 
 	const newUser = new User({ username });
