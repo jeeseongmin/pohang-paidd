6fe0736504f4a33b0d725eee877155de447a4762
21500706 <21500706@handong.edu>
2021-08-15 12:03:31 +0900

    complet navrbar design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,30 +2,176 @@ import React, { useState } from "react";
 import { Route, Link } from "react-router-dom";
 
 const Navbar = () => {
-	const [mouseHover, setMouseHover] = useState(0);
-	const hoverIn = (e) => {
-		setMouseHover(1);
+	const [overMenu, setOverMenu] = useState([false, false, false, false, false]);
+	const hoverIn = (menu) => {
+		const cp = [...overMenu];
+		cp[menu] = true;
+		setOverMenu(cp);
 	};
 
-	const hoverOut = (e) => {
-		setMouseHover(0);
+	const hoverOut = (menu) => {
+		const cp = [...overMenu];
+		cp[menu] = false;
+		setOverMenu(cp);
 	};
 
 	return (
-		<div
-			onMouseOver={hoverIn}
-			onMouseOut={hoverOut}
-			class="w-full px-36 h-16 bg-yellow-200 flex justify-start"
-		>
-			<div class="border border-red-500 flex justify-center items-center">
+		<div class=" w-full px-36 h-16 flex justify-start shadow-xl">
+			<div class="flex justify-center items-center">
 				<img src="/image/logo.png" class="h-10" alt="logo" />
 			</div>
-			<div class="border border-blue-500 flex-1 flex justify-end items-center">
-				<Link class="font-semibold text-xl ml-16">협회소개</Link>
-				<Link class="font-semibold text-xl ml-16">주요사업</Link>
-				<Link class="font-semibold text-xl ml-16">부설기관</Link>
-				<Link class="font-semibold text-xl ml-16">참여마당</Link>
-				<Link class="font-semibold text-xl ml-16">지역복지</Link>
+			<div class="flex-1 flex justify-end items-center">
+				<div
+					onMouseOver={() => hoverIn(0)}
+					onMouseOut={() => hoverOut(0)}
+					class="h-full relative ml-16 flex items-center"
+				>
+					<Link
+						class={
+							"pl-4 font-semibold text-lg " +
+							(overMenu[0] ? "text-blue-500" : "")
+						}
+					>
+						협회소개
+					</Link>
+					<div
+						class={
+							"border border-gray-100 w-36 absolute top-14 shadow-lg flex flex-col bg-white " +
+							(overMenu[0] ? "block" : "hidden")
+						}
+					>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							인사말
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							설립목적
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							연혁
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							조직도
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 hover:bg-gray-100 hover:text-blue-500">
+							오시는 길
+						</Link>
+					</div>
+				</div>
+				<div
+					onMouseOver={() => hoverIn(1)}
+					onMouseOut={() => hoverOut(1)}
+					class="h-full relative ml-16 flex items-center"
+				>
+					<Link
+						class={
+							"pl-4 font-semibold text-lg " +
+							(overMenu[1] ? "text-blue-500" : "")
+						}
+					>
+						주요사업
+					</Link>
+					<div
+						class={
+							"border border-gray-100 w-56 absolute top-14 shadow-lg flex flex-col bg-white " +
+							(overMenu[1] ? "block" : "hidden")
+						}
+					>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							협회사업
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							포항시지적장애인 자립지원센터
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							장애인활동 지원사업
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 hover:bg-gray-100 hover:text-blue-500">
+							방과후활동 지원사업
+						</Link>
+					</div>
+				</div>
+
+				<div
+					onMouseOver={() => hoverIn(2)}
+					onMouseOut={() => hoverOut(2)}
+					class="h-full relative ml-16 flex items-center"
+				>
+					<Link
+						class={
+							"pl-4 font-semibold text-lg " +
+							(overMenu[2] ? "text-blue-500" : "")
+						}
+					>
+						부설기관
+					</Link>
+					<div
+						class={
+							"border border-gray-100 w-36 absolute top-14 shadow-lg flex flex-col bg-white " +
+							(overMenu[2] ? "block" : "hidden")
+						}
+					>
+						<Link class="text-xs text-gray-400 pl-4 py-3  hover:bg-gray-100 hover:text-blue-500">
+							부설기관
+						</Link>
+					</div>
+				</div>
+				<div
+					onMouseOver={() => hoverIn(3)}
+					onMouseOut={() => hoverOut(3)}
+					class="h-full relative ml-16 flex items-center"
+				>
+					<Link
+						class={
+							"pl-4 font-semibold text-lg " +
+							(overMenu[3] ? "text-blue-500" : "")
+						}
+					>
+						참여마당
+					</Link>
+					<div
+						class={
+							"border border-gray-100 w-36 absolute top-14 shadow-lg flex flex-col bg-white " +
+							(overMenu[3] ? "block" : "hidden")
+						}
+					>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							공지사항
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							건의 및 고충상담
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 border-b border-gray-200 hover:bg-gray-100 hover:text-blue-500">
+							후원
+						</Link>
+						<Link class="text-xs text-gray-400 pl-4 py-3 hover:bg-gray-100 hover:text-blue-500">
+							자원봉사
+						</Link>
+					</div>
+				</div>
+				<div
+					onMouseOver={() => hoverIn(4)}
+					onMouseOut={() => hoverOut(4)}
+					class="h-full relative ml-16 flex items-center"
+				>
+					<Link
+						class={
+							"pl-4 font-semibold text-lg " +
+							(overMenu[4] ? "text-blue-500" : "")
+						}
+					>
+						지역복지
+					</Link>
+					<div
+						class={
+							"border border-gray-100 w-36 absolute top-14 shadow-lg flex flex-col bg-white " +
+							(overMenu[4] ? "block" : "hidden")
+						}
+					>
+						<Link class="text-xs text-gray-400 pl-4 py-3 hover:bg-gray-100 hover:text-blue-500">
+							지역복지
+						</Link>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,31 @@
 import React from "react";
 
 const Home = () => {
-	return <div>Home</div>;
+	return (
+		<div class="w-full h-full">
+			{/* section 1 */}
+			<div class="px-16 py-16 bg-purple-100 h-auto flex flex-row">
+				<div class="w-1/2 flex justify-center items-center">
+					<img src="/image/home-img1.png" alt="logo" />
+				</div>
+				<div class="pb-16 flex flex-col justify-end">
+					<h3 class="text-pink-400 mb-6 text-2xl">RENEWAL OPEN</h3>
+					<h3 class="text-3xl mb-2">안녕하세요.</h3>
+					<h3 class="text-3xl mb-2">새롭게 열린</h3>
+					<h3 class="text-3xl mb-2">
+						<b>포항발달장애인협회</b> 입니다.
+					</h3>
+				</div>
+			</div>
+
+			{/* section 2 */}
+			<div></div>
+			{/* section 3 */}
+			<div></div>
+			{/* section 4 */}
+			<div></div>
+		</div>
+	);
 };
 
 export default Home;
